32d9de82484bc51209b4d086a6e9c0bc59c0b754
Thomas Sam-Yin-Yang <thomas.samyinyang@gmail.com>
2021-05-13 17:06:50 -0400

    Added the controllers, and views + Some designs.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,35 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: [:destroy, :destroy]
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    @list = @bookmark.list
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
